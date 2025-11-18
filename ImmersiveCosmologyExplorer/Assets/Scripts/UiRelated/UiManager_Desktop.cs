c1a6c2561aee49e6f4af5ec3f5dd50c05e046e79
Superkart <karthik.ragi15@gmail.com>
2025-11-18 00:39:53 -0600

    Made a basic Menu Loader system

---
+++ ImmersiveCosmologyExplorer/Assets/Scripts/UiRelated/UiManager_Desktop.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class UiManager_Desktop : MonoBehaviour
+{
+    [SerializeField] private GameObject LoadDataPanel;
+    [SerializeField] private GameObject LoadSessionPanel;
+    [SerializeField] private GameObject VisualizingPanel;
+
+}
